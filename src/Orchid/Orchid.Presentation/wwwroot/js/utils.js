af8aa045223b3d8c48e7b46a9f075bc1145c2f31
TeseySTD <skoreiko.mykhailo@chnu.edu.ua>
2026-01-14 00:05:29 +0200

    feat(pagination):Add pages recalculation after resizing.

--- src/Orchid/Orchid.Presentation/wwwroot/js/utils.js
+++ src/Orchid/Orchid.Presentation/wwwroot/js/utils.js
@@ -1,21 +1,43 @@
 ﻿window.utils = {
     resizeObserve: (element, dotNetReference) => {
+        let debounceTimer;
+        let throttleTimer;
+
         const resizeObserver = new ResizeObserver((entries) => {
-            for (let entry of entries) {
-                // Call the C# method 'OnResize'
-                dotNetReference.invokeMethodAsync('OnResize');
+            if (!throttleTimer) {
+                throttleTimer = requestAnimationFrame(() => {
+                    // Call the C# method 'OnResize'
+                    dotNetReference.invokeMethodAsync('OnResize');
+                    throttleTimer = null;
+                });
             }
+
+            clearTimeout(debounceTimer);
+            debounceTimer = setTimeout(() => {
+                // Call the C# method 'OnResizeEnd' after debounce
+                dotNetReference.invokeMethodAsync('OnResizeEnd');
+            }, 500); 
         });
 
         resizeObserver.observe(element);
 
-        // Store the observer on the element to disconnect later
         element._resizeObserver = resizeObserver;
+        element._resizeCleanup = () => {
+             clearTimeout(debounceTimer);
+             cancelAnimationFrame(throttleTimer);
+        };
     },
+
     resizeUnobserve: (element) => {
-        if (element && element._resizeObserver) {
-            element._resizeObserver.disconnect();
-            delete element._resizeObserver;
+        if (element) {
+            if (element._resizeCleanup) {
+                element._resizeCleanup();
+                delete element._resizeCleanup;
+            }
+            if (element._resizeObserver) {
+                element._resizeObserver.disconnect();
+                delete element._resizeObserver;
+            }
         }
     }
-}
+};
