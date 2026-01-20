aa6d506023c2ab8c633c05d2a9b73c474dc8be8c
TeseySTD <skoreiko.mykhailo@chnu.edu.ua>
2026-01-20 15:08:27 +0200

    refactor(core,ui):Remove last name from Author.

--- src/Orchid/Orchid.Core/Models/ValueObjects/Author.cs
+++ src/Orchid/Orchid.Core/Models/ValueObjects/Author.cs
@@ -1,15 +1,10 @@
-﻿
-namespace Orchid.Core.Models.ValueObjects;
+﻿namespace Orchid.Core.Models.ValueObjects;
 
 public class Author
 {
-    private Author(string firstName, string? lastName) => (FirstName, LastName) = (firstName, lastName);
+    private Author(string name) => Name = name;
 
-    public string FirstName { get; }
-    public string? LastName { get; }
+    public string Name { get; }
 
-    public static Author Create(string firstName, string? lastName) => new(firstName, lastName);
-    public static Author Create(string firstName) => new(firstName, null);
-
-    public override string ToString() => LastName is null ? FirstName : $"{FirstName} {LastName}";
+    public static Author Create(string name) => new(name);
 }
